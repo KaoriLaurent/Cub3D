350b2b388bfd83e147ff209e394601d0de53f673
requinch <requinch@student.42.fr>
2022-08-04 01:32:39 +0200

    lol

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing_main.c                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: requinch <requinch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 03:24:43 by requinch          #+#    #+#             */
-/*   Updated: 2022/08/01 04:57:14 by requinch         ###   ########.fr       */
+/*   Updated: 2022/08/04 01:32:03 by requinch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@
 int	main(int ac, char **av)
 {
 	t_world	world;
+	t_all	*a;
 
+	a = malloc(sizeof(t_all));
+	a->vars = malloc(sizeof(t_vars));
 	if (ac != 2)
 		return (error_int_ret(ERR_ARG, 0));
 	if (!parsing(av[1]))
@@ -32,6 +35,11 @@ int	main(int ac, char **av)
 	if (!world.map.raw)
 		return (free_return(world.map.raw, 1));
 	fill_world(&world);
-	print_world(world);
+	window_init(a->vars);
+	a->vars->p = &world.player;
+	a->player = &world.player;
+	mlx_loop_hook(a->vars->mlx, render_next_frame, a);
+	mlx_hook(a->vars->win, 2, 0, my_key_hook, a);
+	mlx_loop(a->vars->mlx);
 	return (free_world(&world));
 }
